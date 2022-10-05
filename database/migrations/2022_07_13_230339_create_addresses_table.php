692b09ee0b509e041723f247d752a0cd442936c8
Alenilson Souza <alenilsonsouza@gmail.com>
2022-10-05 13:06:58 -0300

    first commit

---
+++ database/migrations/2022_07_13_230339_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('zipcode');
+            $table->string('street');
+            $table->string('number', 50)->nullable();
+            $table->string('complement', 150);
+            $table->string('district');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('id_people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
